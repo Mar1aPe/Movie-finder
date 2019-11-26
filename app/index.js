a779abe9ebe23f1e38ea7db99081b64b9289323b
Maria <adresmarii@gmail.com>
2019-11-26 19:06:37 +0100

    Rearrange files

--- app/index.js
+++ app/index.js
@@ -1,66 +1,66 @@
 import "@babel/polyfill";
 import './styles/index.scss';
 
-import { movieInfo, movieNotFound, getDirectors, getActors } from './modules/MovieInfo.js'
+import { movieInfo, movieNotFound, getDirectors, getActors } from './modules/MovieInfo'
 
 // build your app here
-export const app = () => {
-    const button = document.querySelector('button')
-    const input = document.querySelector('input')
-    const loader = document.querySelector('.loader')
-    const rateStar = document.querySelector('.fas')
 
-    // ---------getting Movie Title from user's input------------
+const button = document.querySelector('button')
+const input = document.querySelector('input')
+const loader = document.querySelector('.loader')
+const rateStar = document.querySelector('.fas')
 
-    const titleToSearch = () => {
-        return input.value
-    }
+// ---------getting Movie Title from user's input------------
 
-
-    //------------fetch movie by title from user's input--------
-
-    const getFilm = (movie) => {
-        return fetch(`https://www.omdbapi.com/?apikey=265f0738&t=${movie}`)
-            .then(response => response.json())
-    }
-
-    //------------appending movie details to body----------------
-
-    const insertMovieData = (film) => {
-        loader.style.display = 'flex'
-        setTimeout(function () {
-            loader.style.display = 'none'
-            if (film.Response !== 'False') {
-                getActors(film)
-                getDirectors(film)
-                movieInfo(film)
-                rateStar.style.display = 'visible'
-            } else {
-                movieNotFound()
-            }
-        }, 2000)
-
-    }
-
-    //fuction for eventListener on button 'click' using above functions
-
-    const retrieveData = async () => {
-        const movie = titleToSearch()
-        const film = await getFilm(movie)
-        insertMovieData(film)
-        input.value = ''
-
-    }
-
-    // serach movie on button click
-    button.addEventListener('click', retrieveData)
-
-    // serach movie on "enter" key
-    input.addEventListener('keyup', function (event) {
-        if (event.keyCode === 13) {
-            event.preventDefault();
-            retrieveData()
-        }
-    })
+const titleToSearch = () => {
+    return input.value
 }
+
+
+//------------fetch movie by title from user's input--------
+
+const getFilm = (movie) => {
+    return fetch(`http://www.omdbapi.com/?apikey=265f0738&t=${movie}`)
+        .then(response => response.json())
+}
+
+//------------appending movie details to body----------------
+
+const insertMovieData = (film) => {
+    loader.style.display = 'flex'
+    setTimeout(function () {
+        loader.style.display = 'none'
+        if (film.Response !== 'False') {
+            getActors(film)
+            getDirectors(film)
+            movieInfo(film)
+            rateStar.style.display = 'visible'
+        } else {
+            movieNotFound()
+        }
+    }, 2000)
+
+}
+
+//fuction for eventListener on button 'click' using above functions
+
+const retrieveData = async () => {
+    const movie = titleToSearch()
+    const film = await getFilm(movie)
+    insertMovieData(film)
+    input.value = ''
+
+}
+
+// serach movie on button click
+button.addEventListener('click', retrieveData)
+
+// serach movie on "enter" key
+input.addEventListener('keyup', function (event) {
+    if (event.keyCode === 13) {
+        event.preventDefault();
+        retrieveData()
+    }
+})
+
 
